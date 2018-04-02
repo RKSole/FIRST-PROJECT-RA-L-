589f86a7bc09b53c436dd0d506d620980b0875cf
Raul.dll <rdl.llave@gmail.com>
2018-04-02 20:35:17 +0200

    la criatura esta viva

--- js/game.js
+++ js/game.js
@@ -52,4 +52,5 @@ Game.prototype.draw = function() {
 
 Game.prototype.moveAll = function() {
   this.background.move()
+  this.player.move()
 };
--- js/player.js
+++ js/player.js
@@ -1,16 +1,19 @@
 function Player(game){
-  this.x = 50;
-  this.y = 40;
+  this.x = 40;
+  this.y = 45;
   this.game = game;
-  
 
-  this.width = 20;
-  this.height = 50;
+  this.vy = 10;
+  this.vx = 10;
+
+  this.width = 35;
+  this.height = 35;
 
   this.img = new Image();
-  this.img.src = "images/player.png";
-  this.img.frames = 3;
+  this.img.src = "images/spritesI.png";
+  this.img.frames = 6;
   this.img.frameIndex = 1;
+  this.setListeners();
 }
 
 Player.prototype.draw = function() {
@@ -28,8 +31,23 @@ Player.prototype.draw = function() {
 };
 
 Player.prototype.setListeners = function() {
-
-};
+ document.onkeydown = function(e){
+  switch (e.keyCode) {
+    case 38:
+    this.y -= 10;
+    break;
+    case 40:
+    this.y += 10;
+    break;
+    case 37:
+    this.x -=10;
+    break;
+    case 39:
+    this.x +=10
+    break;
+    }
+  }.bind(this)
+}
 
 Player.prototype.animateImg = function() {
 
